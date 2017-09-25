bd72a98d49d3e48ad8ab70297df5774775e8142f
Grant Smith <Smith9327@Grants-MacBook-Pro.local>
2017-09-25 00:26:59 -0500

    part 3

---
+++ src/part3.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class part3 {
+
+	public class fbArray {
+
+	    public static String[] fizzbuzz(int n) {
+
+	        if (n >= 1) {
+	            String[] array = new String[n];
+
+	            for (int i = 1; i <= n; i++) {
+	                if (i % 3 == 0 && i % 5 != 0) {
+	                    array[i - 1] = "fizz";
+	                } else if (i % 3 != 0 && i % 5 == 0) {
+	                    array[i - 1] = "buzz";
+	                } else if (i % 3 == 0 && i % 5 == 0) {
+	                    array[i - 1] = "fizzbuzz";
+	                } else {
+	                    array[i - 1] = String.valueOf(i);
+	                }
+	            }
+
+	            System.out.println(Arrays.toString(array));
+
+	            return array;
+
+	        } else {
+	            System.out.println("Invalid Number");
+	            return null;
+	        }
+
+
+	    }
+	}
+}
