2acefbfdf01b7206897134ba03fe709b7621d24b
Grant Smith <Smith9327@Grants-MacBook-Pro.local>
2017-09-25 00:14:25 -0500

    part 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,30 @@
 
 public class Main {
 
+	public static void fb(int n) {
+		
+		if( n >= 1) {
+			
+			 for (int i = 1; i <= n; i++) {
+	                if ((i % 3 == 0) && (i % 5 == 0)) {
+	                    System.out.println("fizzbuzz");
+	                } else if (i % 3 == 0) {
+	                    System.out.println("fizz");
+
+	                } else if (i % 5 == 0) {
+	                    System.out.println("buzz");
+
+	                } else {
+	                    System.out.println(i);
+
+	                }
+
+	            }
+			
+		} else 
+			System.out.println("invalid number");
+			System.exit(0);
+		
+	}
+	
 }
