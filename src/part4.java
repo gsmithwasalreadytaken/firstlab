b2bad601a3e06a15551977a7c7807dd9fbcfbd55
Grant Smith <Smith9327@Grants-MacBook-Pro.local>
2017-09-25 00:35:33 -0500

    lab1-part4

---
+++ src/part4.java
@@ -0,0 +1,54 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class part4 {
+
+    public static List<String> fb4(int n) {
+
+        if (n >= 1) {
+        	
+            List<String> list = new ArrayList<String>();
+
+            for (int i = 1; i <= n; i++) {
+            	
+                int f = i % 3;
+                int b = i % 5;
+                
+                if (f == 0 && b != 0) {
+                	
+                   list.add("fizz");
+                   
+                } else 
+                	 	
+                	if (f != 0 && b == 0) {
+                		
+                    list.add("buzz");
+                    
+                } else 
+                	    
+                	if (f == 0 && b == 0) {
+                	
+                   list.add("fizzbuzz");
+                   
+                } else {
+                	
+                    list.add(String.valueOf(i));
+                    
+                }
+            }
+
+            System.out.println(list);
+
+            return list;
+
+        } else {
+        	
+            System.out.println("Invalid Number");
+            return null;
+            
+        }
+
+
+    }
+
+}
